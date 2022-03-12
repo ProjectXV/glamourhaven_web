cd4fa4540603d7fede50762123d0cff68837a0fc
Sau Juliana <julienyange@gmail.com>
2022-03-13 00:42:18 +0300

    Create Login page UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import Home from './pages/Home';
 import { Routes, Route } from 'react-router-dom';
+import Login from './pages/Authentication/Login';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         {/* External Pages */}
         <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
