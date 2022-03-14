c6612c5035464e1f8a475b9d22645ebbcbf9938d
njeveh <njeveh@gmail.com>
2022-03-14 17:44:44 +0300

    remove type from detail

--- backend/glamourhaven/glamour/utils/exceptions.py
+++ backend/glamourhaven/glamour/utils/exceptions.py
@@ -1,6 +1,6 @@
 # utils/custom_exception.py
 from rest_framework import status
-from rest_framework.exceptions import APIException
+from rest_framework.exceptions import APIException, _get_error_details
 
 
 class CustomException(APIException):
@@ -14,12 +14,13 @@ class CustomException(APIException):
             self,
             status_code=status.HTTP_400_BAD_REQUEST,
             code: int = None,
-            detail: str = None,
+            detail=None,
             data=None,
     ):
         self.status_code = status_code
         self.code = self.default_code if code is None else code
-        self.detail = self.default_detail if detail is None else detail
+        self.detail = self.default_detail if detail is None else _get_error_details(
+            detail, code)
 
         if data is None:
             self.data = {"detail": self.detail, "code": self.code}
@@ -27,4 +28,4 @@ class CustomException(APIException):
             self.data = data
 
     def __str__(self):
-        return str(self.code) + self.detail
+        return str(self.code) + self.detail['error']
