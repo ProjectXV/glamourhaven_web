67bb3fddfb5e8519986cef737f0858c148777d2e
njeveh <njeveh@gmail.com>
2022-03-14 17:41:56 +0300

    add client signup and user login paths

--- backend/glamourhaven/glamour/urls.py
+++ backend/glamourhaven/glamour/urls.py
@@ -9,4 +9,10 @@ urlpatterns = [
     # access api end point for user account activation
     re_path(
         r'^activate-account/(?P<uid>[\w-]+)/(?P<token>[\w-]+)/$', views.UserActivationView.as_view()),
+
+    # accesses api end point for creating a new client
+    path('signup', views.CreateClientApiView.as_view(), name='signup'),
+
+    # accesses an api end point for acquiring an expiring auth token
+    path('login', views.CustomAuthToken.as_view(), name='login'),
 ]
